303f31ec256e2734f99311d6d8bbc8c2206b8d60
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-01-17 16:54:33 -0300

    Data / Section

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -34,6 +34,10 @@ INSERT INTO tb_resource (title, description, position, img_uri, external_link, t
 INSERT INTO tb_resource (title, description, position, img_uri, external_link, type, offer_id) 
 	   VALUES ( 'Lives bônus', 'Lives exclusivas', 3, 'https://raw.githubusercontent.com/devsuperior/bds-assets/main/img/student.png', null, 0, 1);
 	   
-/*RESOURCE*/
+/*SECTION*/
 INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id)
 	   VALUES ( 'Capítulo 1', 'Neste capítulo vamos começar', 1, 'https://raw.githubusercontent.com/devsuperior/bds-assets/main/img/student.png', 1, null);
+INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id)
+	   VALUES ( 'Capítulo 2', 'Iniciando os cadastros', 2, 'https://raw.githubusercontent.com/devsuperior/bds-assets/main/img/student.png', 1, 1);
+INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id)
+	   VALUES ( 'Capítulo 3', 'Finalizando os cadastros', 3, 'https://raw.githubusercontent.com/devsuperior/bds-assets/main/img/student.png', 1, 2);
